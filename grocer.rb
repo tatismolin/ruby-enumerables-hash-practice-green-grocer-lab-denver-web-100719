4bcdb6485bf2c4bb467d66147cf158295b0f6a72
Tatiana Smolin <tatismolin@gmail.com>
2019-09-23 01:11:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -25,14 +25,14 @@ def apply_coupons(cart, coupons)
         :count => 1
         }
       }
-      
+
       if hash["#{item} W/COUPON"].nil?
         hash.merge!(temp)
       else
         hash["#{item} W/COUPON"][:count] += 1
         #hash["#{item} W/COUPON"][:price] += coupon_hash[:cost]
       end
-      
+
       hash[item][:count] -= coupon_hash[:num]
     end
   end
@@ -52,13 +52,13 @@ def checkout(items, coupons)
   cart = consolidate_cart(items)
   cart1 = apply_coupons(cart, coupons)
   cart2 = apply_clearance(cart1)
-  
+
   total = 0
-  
+
   cart2.each do |name, price_hash|
     total += price_hash[:price] * price_hash[:count]
   end
-  
+
   total > 100 ? total * 0.9 : total
-  
+
 end
