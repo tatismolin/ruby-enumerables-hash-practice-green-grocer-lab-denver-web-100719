10cabbaa6540db3c00a53ff2b58eed71caa76116
Tatiana Smolin <tatismolin@gmail.com>
2019-09-23 00:32:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -43,9 +43,24 @@ end
 
 def apply_clearance(cart)
   # code here
+  cart.each do |item, price_hash|
+    if price_hash[:clearance] == true
+      price_hash[:price] = (price_hash[:price] * 0.8).round(2)
+    end
+  end
+  cart
 end
 
 
 def checkout(cart, coupons)
   # code here
+  cart = consolidate_cart(items)
+  cart1 = apply_coupons(cart, coupons)
+  cart2 = apply_clearance(cart1)
+  total = 0
+  
+  cart2.each do |name, price_hash|
+    total += price_hash[:price] * price_hash[:count]
+  end
+  total > 100 ? total * 0.9 : total
 end
