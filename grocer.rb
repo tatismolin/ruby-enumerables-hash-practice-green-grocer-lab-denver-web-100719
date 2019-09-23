0841d65ac0c6a0446b47793c62d7aea461a6865f
Tatiana Smolin <tatismolin@gmail.com>
2019-09-23 00:45:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -32,6 +32,9 @@ def apply_coupons(cart, coupons)
         hash["#{item} W/COUPON"][:count] += 1
         #hash["#{item} W/COUPON"][:price] += coupon_hash[:cost]
       end
+
+      hash[item][:count] += coupon_hash[:num]
+    end
   end
   hash
 end
